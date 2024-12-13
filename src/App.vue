706f7e9cfd7db8c184a843a8f717f8d6b297e144
baduyifei <baduyifei@qq.com>
2024-12-13 18:41:22 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <!-- @format -->
 
 <template>
+	<!-- template  -->
 	<div id="app">
 		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
 	</div>
 </template>
 
 <script>
-	import HelloWorld from './components/HelloWorld.vue';
-
 	export default {
 		name: 'App',
 		components: {
@@ -20,11 +18,6 @@
 
 <style>
 	#app {
-		font-family: Avenir, Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
+		background-color: pink;
 	}
 </style>
