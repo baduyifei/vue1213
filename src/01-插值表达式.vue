a673a7723896c7b0a4a6e25e0f1c438e767e8d8e
baduyifei <baduyifei@qq.com>
2024-12-16 09:39:11 +0800

    change

---
+++ src/01-插值表达式.vue
@@ -0,0 +1,13 @@
+<!-- @format -->
+
+<template>
+	<div>
+		<h1>v-bind</h1>
+	</div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 /** @format */
 
+// 若是没有开启Devtools工具，在开发环境中开启，在生产环境中关闭
+Vue.config.devtools = true;
+// if (process.env.NODE_ENV == 'development') {
+// 	Vue.config.devtools = true;
+// } else {
+// 	Vue.config.devtools = false;
+// }
 import Vue from 'vue';
 import App from './App.vue';
 
